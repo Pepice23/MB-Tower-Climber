34a3ca2c9ef02b90c4573d736a21f7b4a85ce6dd
Molnár József <mojo12984@gmail.com>
2023-03-07 10:09:02 +0100

    moved Boss timing to BattleService

--- Services/GameService.cs
+++ Services/GameService.cs
@@ -7,15 +7,15 @@
         public int FloorCount { get; set; } = 1;
         public int MonsterCount { get; set; } = 0;
         public int TotalMonsterCount { get; set; } = 0;
-        public int BossTimer { get; set; } = 30;
+
 
         public string FloorCountString => $"width:{FloorCount}%";
         public string MonsterCountString => $"width:{_monsterCountWidth}%";
-        public string BossTimerString => $"width:{_bossTimerWidth}%";
+
         public string BossText => $"Boss of the {FloorCount} floor";
 
         private int _monsterCountWidth;
-        private int _bossTimerWidth = 100;
+
 
         public void AddFloor()
         {
