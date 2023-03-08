1db6b8bf72091aa6db6a340c3bd792dcb8deadaf
Molnár József <mojo12984@gmail.com>
2023-03-08 11:10:34 +0100

    created equipment for the player

---
+++ Services/EquipmentService.cs
@@ -0,0 +1,14 @@
+﻿using MB_Tower_Climber.Helpers;
+
+namespace MB_Tower_Climber.Services
+{
+    public class EquipmentService
+    {
+        public event Action OnChange;
+
+        public  Weapon NewWeapon;
+        public Weapon EquippedWeapon = new Weapon(1);
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
