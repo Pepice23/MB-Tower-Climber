a095fc6cabe163ea993c49bc8b390adfd5d746d6
Molnár József <mojo12984@gmail.com>
2023-03-08 11:10:37 +0100

    Create Weapon.cs

---
+++ Helpers/Weapon.cs
@@ -0,0 +1,106 @@
+﻿namespace MB_Tower_Climber.Helpers
+{
+    public class Weapon
+    {
+
+        public string Name;
+        public int Quality;
+        public int Level;
+        public int PerClickDamage;
+        public int PerSecondDamage;
+        public int Price;
+        public string PicturePath;
+
+        public Weapon(int Level)
+        {
+            Quality = GetRandomRarity();
+            Name = GetWeaponName(Quality);
+            this.Level = Level;
+            PerClickDamage = GetRandomStat(Level, Quality);
+            PerSecondDamage = GetRandomStat(Level, Quality);
+            Price = Level * Quality;
+            PicturePath = GetRandomImage();
+        }
+
+        Random random = new Random();
+        int roll;
+
+
+        enum Rarity
+        {
+            Poor = 1,
+            Uncommon,
+            Rare,
+            Epic,
+            Legendary
+        }
+
+        void DiceRoll()
+        {
+            roll = random.Next(1, 101);
+        }
+
+        int GetRandomRarity()
+        {
+            DiceRoll();
+            if (roll <= 40)
+            {
+                return Quality = (int)Rarity.Poor;
+            }
+            if (roll > 40 && roll <= 75)
+            {
+                return Quality = (int)Rarity.Uncommon;
+            }
+            if (roll > 75 && roll <= 85)
+            {
+                return Quality = (int)Rarity.Rare;
+            }
+            if (roll > 85 && roll <= 98)
+            {
+                return Quality = (int)Rarity.Epic;
+            }
+            if (roll > 98)
+            {
+                return Quality = (int)Rarity.Legendary;
+            }
+            return 1;
+        }
+
+        string GetWeaponName(int Quality)
+        {
+            if (Quality == 1)
+            {
+                return Name = "Rusty Weapon";
+            }
+            if (Quality == 2)
+            {
+                return Name = "Regular Weapon";
+            }
+            if (Quality == 3)
+            {
+                return Name = "Fine Weapon";
+            }
+            if (Quality == 4)
+            {
+                return Name = "Master Weapon";
+            }
+            if (Quality == 5)
+            {
+                return Name = "Legendary Weapon";
+            }
+            return Name;
+        }
+
+        int GetRandomStat(int level, int Quality)
+        {
+            return level * Quality + roll;
+        }
+
+        string GetRandomImage()
+        {
+            int RandomNumber = random.Next(1, 16);
+            return $"assets/loot/weapons/weapon{RandomNumber}.jpeg";
+        }
+    }
+}
+
