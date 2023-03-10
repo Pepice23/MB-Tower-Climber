ed1a704b3cd1733812fc37ba89d0bc85f767028e
Molnár József <mojo12984@gmail.com>
2023-03-10 13:29:53 +0100

    Create PetUpgrade.cs

---
+++ Helpers/PetUpgrade.cs
@@ -0,0 +1,26 @@
+﻿namespace MB_Tower_Climber.Helpers
+{
+    public class PetUpgrade
+    {
+        public string PetName { get; set; }
+        public string Description { get; set; }
+        public string PicturePath { get; set; }
+        public int Cost { get; set; }
+        public int Level { get; set; }
+        public int MaxLevel { get; set; }
+        public int DamageMultiplier { get; set; }
+
+        public PetUpgrade(string petName, string description, string picturePath, int cost, int level, int maxLevel, int damageMultiplier)
+        {
+            PetName = petName;
+            Description = description;
+            PicturePath = picturePath;
+            Cost = cost;
+            Level = level;
+            MaxLevel = maxLevel;
+            DamageMultiplier = damageMultiplier;
+        }
+    }
+
+
+}
